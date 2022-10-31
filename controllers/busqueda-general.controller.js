9ac25b04c032a546b2fe045064feac39dc169a3f
Orel Perez Camejo <orelperezcamejo@gmail.com>
2022-10-31 23:28:04 +0100

    Busqueda general y por coleccion

--- controllers/busqueda-general.controller.js
+++ controllers/busqueda-general.controller.js
@@ -1,14 +1,47 @@
+const Usuario = require('../models/usuario.model');
+const Medico = require('../models/medico.model');
+const Hospital = require('../models/hospital.model');
 
 
+const getAll = async (req, res) => {
+
+    const busqueda = req.params.busqueda;
+    const regex = new RegExp(busqueda, 'i');
 
 
-const getAll = (req, res) => {
+    const [usuarios, medicos, hospitales] = await Promise.all([Usuario.find({nombre: regex}), Medico.find({nombre: regex}), Hospital.find({nombre: regex}),])
 
-    const parametro = req.params.busqueda
-    res.json({message: 'holaaaa', parametro});
+    res.json({usuarios, medicos, hospitales});
+}
+
+const getColeccion = async (req, res) => {
+
+    const tabla = req.params.tabla;
+    const busqueda = req.params.busqueda;
+    const regex = new RegExp(busqueda, 'i');
+    let data = [];
+
+    switch (tabla) {
+        case 'medicos':
+            data = await Medico.find({nombre: regex}).populate('usuario', 'nombre img')
+                .populate('hospital', 'nombre img');
+            break;
+
+        case 'hospitales':
+            data = await Hospital.find({nombre: regex}).populate('usuario', 'nombre img');
+            break;
+
+        case 'usuarios':
+            data = await Usuario.find({nombre: regex});
+            break;
+
+        default:
+            return res.status(400).json({message: 'La tabla tiene que ser usuarios/medicos/hospiatles'});
+
+    }
+
+    res.status(200).json({data});
 }
 
 
-
-
-module.exports = {getAll}
+module.exports = {getAll, getColeccion}
--- routes/busqueda-general.routes.js
+++ routes/busqueda-general.routes.js
@@ -6,11 +6,13 @@ const {check} = require('express-validator');
 const {validarJWT} = require('../middlewares/validar-jwt');
 const {validarCampos} = require('../middlewares/validar-campos');
 
-const {getAll} = require('../controllers/busqueda-general.controller');
+const {getAll, getColeccion} = require('../controllers/busqueda-general.controller');
 
 const router = Router();
 
-router.get('/:busqueda', [validarJWT], getAll);
+router.get('/:busqueda', validarJWT, getAll);
+
+router.get('/coleccion/:tabla/:busqueda', validarJWT, getColeccion);
 
 
 module.exports = router;
